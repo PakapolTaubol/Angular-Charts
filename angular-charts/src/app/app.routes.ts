11c8b1c1787dbb4490aa13d8954fc4602c514b4d
PakapolTaubol <pakapol.taubol@mail.kmutt.ac.th>
2024-02-24 22:10:05 +0700

    [new] Init Project

---
+++ angular-charts/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ChartComponent } from './chart/chart.component';
+
+export const routes: Routes = [
+    {
+        path: 'chart',
+        component: ChartComponent,
+    }
+];
